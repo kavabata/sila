35baeba042ed246f7915687052d0ed922bf7ca3c
Sasha Shuplat <sasha@teacherspayteachers.com>
2024-05-25 13:26:19 +0200

    New Page: Form

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+include('includes/config.php');
+include('includes/section.php');
+
+$page = $translations['pages'][$pageName];
+
+include('includes/header.php');
+?>
+<div class="content">
+  FORM
+</div>
+<?php include('includes/footer.php'); ?>
